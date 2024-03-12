1352c9c48b39ed4c2c6452ed592c892632dc77aa
salimsalim8 <134438035+salimsalim8@users.noreply.github.com>
2024-03-12 14:58:23 +0300

    First Commit
    2023

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ["./file/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
